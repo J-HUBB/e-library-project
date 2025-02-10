52a6e2fc1a1337f8f7f3091bdd8d8d9654e619af
J-HUBB <lowlifeenterprizes@yahoo.com>
2025-02-10 15:25:07 -0500

    e-library

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-
-import Featured from './components/Featured.jsx'
-import Highlights from './components/Highlights.jsx'
-import Landing from './components/Landing.jsx'
+import Footer from './components/Footer.jsx'
 import Nav from './components/Nav.jsx'
-
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Books from './pages/Books.jsx'
+import {books} from "./data";
+import BookInfo from './pages/BookInfo.jsx'
 
 function App() {
   return (
+    <Router>
     <div className='App'>
      <Nav />
-     <Landing />
-     <Highlights />
-     <Featured />
+     <Route path="/" exact component={Home} />
+     <Route path="/books" exact render={() => <Books books={books} />}/>
+     <Route path="/books/1" render={() => <BookInfo books={books} />}/>
+     <Footer />
     </div>
-  )
+    </Router>
+  );
 }
 
 export default App
--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,35 +1,24 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar, faStarHalfAlt } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import Price from "./Price";
 
-const Book = (props) => {
+const Book = ({ book }) => {
   return (
     <div className="book">
-      <a href="">
+      <Link to="/books/1">
         <figure className="book__img--wrapper">
-          <img
-            src="https://m.media-amazon.com/images/I/81bsw6fnUiL.jpg"
-            alt=""
-            className="book__img"
-          />
+          <img src={book.url} alt="" className="book__img" />
         </figure>
-      </a>
+      </Link>
       <div className="book__title">
-        <a href="/" className="book__title--link">
-          Atomic Habits
-        </a>
+        <Link to="/books/1" className="book__title--link">
+          {book.title}
+        </Link>
       </div>
-      <div className="book__ratings">
-        <FontAwesomeIcon icon={faStar} />
-        <FontAwesomeIcon icon={faStar} />
-        <FontAwesomeIcon icon={faStar} />
-        <FontAwesomeIcon icon={faStar} />
-        <FontAwesomeIcon icon={faStarHalfAlt} />
-      </div>
-      <div className="book__price">
-        <span className="book__price--normal">$15.00</span>
-        $10.00
-      </div>
+        <Rating rating={book.rating} />
+        <Price salePrice={book.salePrice} originalPrice={book.originalPrice}/>
     </div>
   );
 };
